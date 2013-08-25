b915336d325b9007618eade301454e2af9bc2150
ajlopez <webmaster@ajlopez.com>
2013-08-25 08:02:27 -0300

    Column first test

---
+++ Src/SharpBase.Tests/ColumnTests.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpBase.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ColumnTests
+    {
+        [TestMethod]
+        public void CreateColumn()
+        {
+            Column column = new Column("Address");
+            Assert.AreEqual("Address", column.Name);
+        }
+    }
+}
---
+++ Src/SharpBase/Column.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpBase
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Column
+    {
+        private string name;
+
+        public Column(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
